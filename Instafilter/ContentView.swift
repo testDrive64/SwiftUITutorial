e89151650efc1be87b6ccfbcb6aeb2b724a3e72f
testDrive64 <com9on@me.com>
2025-04-05 12:19:05 +0200

    ConfirmationView with different options

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,12 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Hello, world!") {
+                showingConfirmation = true
+            }
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+                .blur(radius: blurAmount)
+                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                    Button("Red") { backgroundColor = .red}
+                    Button("Green") { backgroundColor = .green }
+                    Button("Blue") { backgroundColor = .blue }
+                    Button("Yellow") { backgroundColor = .yellow }
+                    Button("Orange") { backgroundColor = .orange }
+                    Button("White") { backgroundColor = .white }
+                    Button("Black") { backgroundColor = .black }
+                    Button("Cancel", role: .cancel) {}
+                } message: {
+                    Text("Select a new color")
+                }
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New value is \(newValue)")
+                }
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
         .padding()
     }
