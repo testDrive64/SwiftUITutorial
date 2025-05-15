cf3e8725757de37e4b94026672d81ba920c0db66
testDrive64 <com9on@me.com>
2025-05-15 21:24:16 +0200

    add images to the AccessibilitySandbox proj

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Image(decorative: "character")
+            }
+            .padding()
     }
 }
 
