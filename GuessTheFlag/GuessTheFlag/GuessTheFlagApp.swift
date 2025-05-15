c746c7ea385c269651b4a3068437fff831716d3c
testDrive64 <com9on@me.com>
2025-05-15 21:46:26 +0200

    Repair GuessTheFlag, add accessibility

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Norman on 30.01.25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
