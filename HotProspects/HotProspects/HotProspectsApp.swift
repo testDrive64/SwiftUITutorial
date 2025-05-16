39269afbade318f8ca1b55289bd9d73f69d9e7b4
testDrive64 <com9on@me.com>
2025-05-17 01:29:04 +0200

    Init HotProspects project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Norman on 17.05.25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
