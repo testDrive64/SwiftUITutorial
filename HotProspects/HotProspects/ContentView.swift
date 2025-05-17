5b4bbbdc834680020801a9bea01d6c6c039c3269
testDrive64 <com9on@me.com>
2025-05-18 00:07:16 +0200

    Result object

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -11,8 +11,13 @@ struct ContentView: View {
     let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
     
     @State private var selection = Set<String>()
+    @State private var selectedTab = "One"
+    @State private var output = ""
+    
     
     var body: some View {
+        
+        
         List(users, id: \.self, selection: $selection) { user in
             Text(user)
         }
@@ -20,6 +25,49 @@ struct ContentView: View {
             Text("You selected \(selection.formatted())")
         }
         EditButton()
+        
+        Button("Show Tab 2") {
+            selectedTab = "Two"
+        }
+        .tabItem{
+            Label("One", systemImage: "star")
+        }
+        
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+        
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+                .tag("One")
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+            case .success(let str):
+                output = str
+            case .failure(let error):
+                output = "Error \(error.localizedDescription)"
+        }
     }
 }
 
